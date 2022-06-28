72e88bfae623611e5b8ceefda6d3a8ce23a9374e
Nathan Presley <npresley13@gmail.com>
2022-06-28 09:55:27 -0400

    added data and flatlist is pulling correctly

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+import { ListItem } from './components/ListItem';
+import { SAMPLE_DATA } from './assets/data/sampleData';
 
 export default function App() {
   return (
@@ -9,6 +12,20 @@ export default function App() {
         <StatusBar style="auto" />
       </View>
       <View style={styles.divider}/>
+
+      <FlatList 
+        keyExtractor={(item) => item.id}
+        data={SAMPLE_DATA}
+        renderItem={({ item }) => (
+          <ListItem 
+            name={item.name}
+            symbol={item.symbol}
+            currentPrice={item.current_price}
+            priceChangePercentage7d={item.price_change_percentage_7d_in_currency}
+            logoUrl={item.image}
+          />
+        )}
+      />
     </View>
   );
 }
@@ -29,5 +46,7 @@ const styles = StyleSheet.create({
   divider: {
     height: StyleSheet.hairlineWidth,
     backgroundColor: '#A9ABB1',
+    marginHorizontal: 16,
+    marginTop: 16,
   }
 });
