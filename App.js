82fb70f1aec887306d17e0b1c86b8678b1ae6cf2
Nathan Presley <npresley@strong-hold.com>
2022-06-29 15:35:28 -0400

    created listheadercomponent on flatlist

--- App.js
+++ App.js
@@ -1,32 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { FlatList, StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View, SafeAreaView } from 'react-native';
 import { ListItem } from './components/ListItem';
 import { SAMPLE_DATA } from './assets/data/sampleData';
 
+import {
+  BottomSheetModal,
+  BottomSheetModalProvider,
+} from '@gorhom/bottom-sheet';
+
+const ListHeader = () => (
+  <>
+    <View style={styles.titleWrapper}>
+      <Text style={styles.largeTitle}>Markets</Text>
+      <StatusBar style="auto" />
+    </View>
+    <View style={styles.divider}/>
+  </>
+)
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View style={styles.titleWrapper}>
-        <Text style={styles.largeTitle}>Markets</Text>
-        <StatusBar style="auto" />
-      </View>
-      <View style={styles.divider}/>
-
-      <FlatList 
-        keyExtractor={(item) => item.id}
-        data={SAMPLE_DATA}
-        renderItem={({ item }) => (
-          <ListItem 
-            name={item.name}
-            symbol={item.symbol}
-            currentPrice={item.current_price}
-            priceChangePercentage7d={item.price_change_percentage_7d_in_currency}
-            logoUrl={item.image}
-          />
-        )}
-      />
-    </View>
+    <BottomSheetModalProvider>
+      <SafeAreaView style={styles.container}>
+        <FlatList 
+          keyExtractor={(item) => item.id}
+          data={SAMPLE_DATA}
+          renderItem={({ item }) => (
+            <ListItem 
+              name={item.name}
+              symbol={item.symbol}
+              currentPrice={item.current_price}
+              priceChangePercentage7d={item.price_change_percentage_7d_in_currency}
+              logoUrl={item.image}
+            />
+          )}
+          ListHeaderComponent={<ListHeader />}
+        />
+      </SafeAreaView>
+    </BottomSheetModalProvider>
   );
 }
 
